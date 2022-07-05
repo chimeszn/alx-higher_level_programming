7a8e4fa4395db4254277bfd71a9238ce724688a1
Oluchime Promise <thesixthking567@gmail.com>
2022-07-05 12:57:21 -0700

    fixed task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
-"""Module 14-pascal_triangle.
-Returns a list of lists of integers
-representing the Pascal’s triangle of n.
+"""
+Module 14-pascal_triangle
+Contains function that returns int lists of pascal triangle of any given size
 """
 
 
 def pascal_triangle(n):
-    """Returns the pascal triangle of n.
-    Args:
-        - n: size of the triangle (rows)
-    Returns: a list of list of integers
     """
-
+    Return:
+        empty list [] if n <= 0
+        if n is 7, we should expect:
+            [1]
+            [1, 1]
+            [1, 2, 1]
+            [1, 3, 3, 1]
+            [1, 4, 6, 4, 1]
+            [1, 5, 10, 10, 5, 1]
+            [1, 6, 15, 20, 15, 6, 1]
+    """
     if n <= 0:
         return []
+    if n == 1:
+        return [[1]]
 
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
+    triangle = [[1]]
+    for rows in range(n-1):
+        triangle.append([a+b for a, b
+                         in zip([0] + triangle[-1], triangle[-1] + [0])])
+    return triangle
 
-    for i in range(1, n):
-        l[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
-            else:
-                l[i][j] = l[i - 1][0]
-    return l
+# C implementation of the last Pythonic loop
+#
+#    triangle = [[1]]
+#    for rows in range(n-1):
+#        l = [1]
+#        for i in range(rows):
+#            l.append(triangle[-1][i] + triangle[-1][i+1])
+#        l.append(1)
+#        triangle.append(l)
+#    return triangle
